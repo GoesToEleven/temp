55e585a5600fb46889e0244804124293bcdbc651
Todd McLeod <tuddleymc@gmail.com>
2019-08-11 18:21:06 -0700

    wassup

---
+++ 001/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+
+	n := "Todd"
+	fmt.Println(n)
+
+	n = "T. Scott"
+	fmt.Println(n)
+
+	n = "Jack"
+	fmt.Println(n)
+
+	n = "Jackson"
+	fmt.Println(n)
+}
